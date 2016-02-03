49df61730bef013ad2dbdf135ceaed58025dc239
David Whitaker <damwhit@gmail.com>
2016-02-03 16:52:44 -0700

    added another user creates feature test

--- test/features/user_creates_task_test.rb
+++ test/features/user_creates_task_test.rb
@@ -26,4 +26,19 @@ class UserCreatesTaskTest < Minitest::Test
       assert page.has_content?("pizza")
     end
   end
+
+  def test_task_creation_with_valid_attributes
+    visit '/tasks/new'
+
+    fill_in 'task[title]', with: 'Example Task'
+    fill_in 'task[description]', with: 'Example Description'
+    click_button 'Submit'
+
+    assert_equal '/tasks', current_path
+
+    within '#tasks' do
+      assert page.has_content? 'Example Task'
+    end
+  end
+  
 end
